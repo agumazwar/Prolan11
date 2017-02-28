c49aea0eb8d6a6dd95a2cd06d733d66b5c22848d
Agum Gumelar Azwar <agum.azwar@gmail.com>
2017-02-28 17:27:33 +0700

    Add files via upload

---
+++ Pertemuan 2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+	public static void main(String[] args){
+		System.out.println("Nama   : Agum Gumelar Azwar");
+		System.out.println("NIM    : 10115192");
+		System.out.println("Alamat : Lubuk Basung ");
+	}
+}
