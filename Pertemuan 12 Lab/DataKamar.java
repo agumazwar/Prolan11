45f53653d5bd3f73d9e66d68d7cb3851e5e33f87
Agum Gumelar Azwar <agum.azwar@gmail.com>
2017-06-01 16:28:03 +0700

    Add files via upload

---
+++ Pertemuan 12 Lab/DataKamar.java
@@ -0,0 +1,26 @@
+public class DataKamar {
+	
+	public void NamaArray(){
+		 String[] ket;
+	
+	ket = new String[5];
+	ket[0]="Agum Gumelar Azwar";
+	ket[1]="Lubuk Basung 11 Juli 1997 ";
+	ket[2]="2015";
+	ket[3]="Lubuk Basung Sumatra Barat";
+	ket[4]="Teknik Informatika";
+	
+	System.out.println("\n=====================Data Penghuni Kamar=========================");
+	System.out.println("Nama Penghuni Kamar      	:" +ket[0]);
+	System.out.println("Tempat Tanggal Lahir		:" +ket[1]);
+	System.out.println("Tahun Masuk Kuliah		:" +ket[2]);
+	System.out.println("Jurusan				:" +ket[3]);
+	System.out.println("Daerah Asal			:" +ket[4]);
+	System.out.println("\n=================================================================");
+	
+	}
+	
+	
+	
+	
+}
---
+++ Pertemuan 12 Lab/isidata.java
@@ -0,0 +1,19 @@
+public class isidata implements datalengkap {
+
+	public void NamaLengkap() {
+		System.out.println("Agum Gumelar Azwar");
+	}
+	public void Ttl() {
+		System.out.println("Lubuk Basung / 11 Juli 1997");
+	}
+	public void Alamat() {
+		System.out.println("Jalan Cisitu Lama 1 No 1 Kamar No 5");
+	}
+	public void Pekerjaan(){
+		System.out.println("Mahasiswa");
+	}
+	public void UangBulanan(){
+		System.out.println("Rp. 1.500.000");
+	}
+	
+}
