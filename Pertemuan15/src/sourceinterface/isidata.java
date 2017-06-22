f3fbdcd30975c109de7db7c132430cd93724c63c
Agum Gumelar Azwar <agum.azwar@gmail.com>
2017-06-23 06:34:06 +0700

    Add files via upload

---
+++ Pertemuan15/src/sourceinterface/isidata.java
@@ -0,0 +1,26 @@
+
+package sourceinterface;
+public class isidata implements datalengkap {
+
+        @Override
+	public void NamaLengkap() {
+		System.out.println("Agum Gumelar Azwar");
+	}
+        @Override
+	public void Ttl() {
+		System.out.println("Lubuk Basung / 11 Juli 1997");
+	}
+        @Override
+	public void Alamat() {
+		System.out.println("Jalan Cisitu Lama 1 No 1 Kamar No 5");
+	}
+        @Override
+	public void Pekerjaan(){
+		System.out.println("Mahasiswa");
+	}
+        @Override
+	public void UangBulanan(){
+		System.out.println("Rp. 1.500.000");
+	}
+	
+}
